327701e2e255fdb273709cfe5cc7c68378d17b7d
chiou <bluebeastmight@gmail.com>
2023-07-21 10:33:41 +0800

    first commit

---
+++ src/main/java/icu/chiou/mlflavordiscovery/MlFlavorDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package icu.chiou.mlflavordiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlFlavorDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlFlavorDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icu/chiou/mlflavordiscovery/MlFlavorDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package icu.chiou.mlflavordiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlFlavorDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
